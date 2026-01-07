ba871e607c903c9e01e3e090b31f6d4434ee6878
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2026-01-07 23:45:29 +0100

    upload a video and store it in s3

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,58 @@
-import { respondWithJSON } from "./json";
+import type { BunRequest } from "bun";
+import path from "path";
 
 import { type ApiConfig } from "../config";
-import type { BunRequest } from "bun";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { respondWithJSON } from "./json";
+import { getBearerToken, validateJWT } from "../auth";
+import { getVideo, updateVideo } from "../db/videos";
+import { getAssetPath } from "./assets";
+import { getS3URL, uploadVideoToS3 } from "../s3";
+import { rm } from "fs/promises";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
-  return respondWithJSON(200, null);
+  const MAX_UPLOAD_SIZE = 1 << 30; // 1GB
+
+  const { videoId } = req.params as { videoId?: string };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  console.log("uploading video for video", videoId, "by user", userID);
+
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Video not found");
+  }
+  if (video.userID !== userID) {
+    throw new UserForbiddenError("Not authorized to update this video");
+  }
+
+  const formData = await req.formData();
+  const file = formData.get("video");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("video file missing");
+  }
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Video exceeds the maximum allowed size of 1GB");
+  }
+  if (file.type !== "video/mp4") {
+    throw new BadRequestError("Invalid file type. Only MP4 allowed");
+  }
+
+  const tempFilePath = path.join("/tmp", `${videoId}.mp4`);
+  await Bun.write(tempFilePath, file);
+
+  const filename = getAssetPath(file.type);
+  await uploadVideoToS3(cfg, filename, tempFilePath, file.type);
+
+  video.videoURL = getS3URL(cfg, filename);
+  updateVideo(cfg.db, video);
+
+  await Promise.all([rm(tempFilePath, { force: true })]);
+
+  return respondWithJSON(200, video);
 }
---
+++ src/s3.ts
@@ -0,0 +1,16 @@
+import type { ApiConfig } from "./config";
+
+export function getS3URL(cfg: ApiConfig, filename: string) {
+  return `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${filename}`;
+}
+
+export async function uploadVideoToS3(
+  cfg: ApiConfig,
+  key: string,
+  processesFilePath: string,
+  contentType: string,
+) {
+  const s3file = cfg.s3Client.file(key, { bucket: cfg.s3Bucket });
+  const videoFile = Bun.file(processesFilePath);
+  await s3file.write(videoFile, { type: contentType });
+}
